befce844d39fda567f29108f63dae8895c776a8b
semtexzv <mhornick@redhat.com>
2019-11-21 15:35:22 +0100

    Use ocdeployer for openshift deployments

--- listener/listener.go
+++ listener/listener.go
@@ -14,15 +14,17 @@ var (
 func configure() {
 	topic := utils.GetenvOrFail("UPLOAD_TOPIC")
 	kafkaAddress := utils.GetenvOrFail("KAFKA_HOST")
-	utils.Log().Info("Connecting to ", kafkaAddress, " listening for ", topic)
+	kafkaPort := utils.GetIntEnvOrFail("KAFKA_PORT")
+	kafkaGroup := utils.GetenvOrFail("KAFKA_GROUP")
+
+	utils.Log().Info("Connecting to ", kafkaAddress, ":", kafkaPort, " listening for ", topic)
 
 	uploadReader = kafka.NewReader(kafka.ReaderConfig{
-		Brokers:   []string{kafkaAddress},
-		Topic:     topic,
-		GroupID:   "spm",
-		Partition: 0,
-		MinBytes:  10e3, // 10KB
-		MaxBytes:  10e6, // 10MB
+		Brokers:  []string{kafkaAddress + ":"+ string(kafkaPort)},
+		Topic:    topic,
+		GroupID:  kafkaGroup,
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
 	})
 
 }
@@ -33,7 +35,6 @@ func shutdown() {
 		utils.Log("err", err.Error()).Error("unable to shutdown Kafka reader")
 	}
 }
-
 
 func RunListener() {
 	utils.Log().Info("listener starting")
@@ -52,7 +53,7 @@ func RunListener() {
 			utils.Log("err", err.Error()).Error("unable to read message from Kafka reader")
 			return
 		}
-		utils.Log().Info("Received message", m)
+		utils.Log().Info("Received message", string(m.Value))
 
 	}
 }
