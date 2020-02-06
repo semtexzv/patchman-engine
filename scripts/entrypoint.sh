c4b7ce6edcb20bcd5a95aec0a2d0fc1465cb3443
Michal Hornicky <mhornick@redhat.com>
2020-02-06 14:21:24 +0100

    Force running pre-built binaries

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,9 +2,9 @@
 
 COMPONENT=$1
 # This script is launched inside the /go/src/app working directory
-if [[ -n $GORUN ]]; then
+#if [[ -n $GORUN ]]; then
   # Running using 'go run'
-  ./scripts/wait-for-services.sh go run main.go $COMPONENT
-else
+#  ./scripts/wait-for-services.sh go run main.go $COMPONENT
+#else
   ./scripts/wait-for-services.sh ./main $COMPONENT
-fi
+#fi
